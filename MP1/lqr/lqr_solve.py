3fd64a2f53d6edb00de67accb861cc6c5456d912
Shuban Iyer <90860963+s1oly@users.noreply.github.com>
2026-05-31 09:00:41 -0400

    Developed LQR Controller for Classical System -MP1

--- MP1/lqr/lqr_solve.py
+++ MP1/lqr/lqr_solve.py
@@ -22,7 +22,7 @@ class LQRControl(object):
         u = self.action_space.sample()
         # TODO: Once you finish implementing LQRSolver, uncomment the following
         # line to use it to control the system 
-        # u = self.lqr.get_control(state, self.step)
+        u = self.lqr.get_control(state, self.step)
         self.step += 1
         return u
 
@@ -33,12 +33,20 @@ class LQRSolver(object):
 
     def solve(self):
         # TODO: Implement the solver here, i.e. compute the time-varying K_i and P_i matrices
-        pass
+        P_i = {}
+        P_i[self.T] = self.Q
+        K_i = {}
+        for i in range (1, self.T + 1):
+            K_i[self.T - i] = np.linalg.inv(self.R + self.B.T @ P_i[self.T - i + 1] @ self.B) @ self.B.T @ P_i[self.T - i + 1] @ self.A
+            P_i[self.T - i] = self.Q + self.A.T @ P_i[self.T - i + 1] @ self.A - self.A.T @ P_i[self.T - i + 1] @ self.B @ np.linalg.inv(self.R +self.B.T @ P_i[self.T - i + 1] @ self.B) @ self.B.T @ P_i[self.T - i + 1] @ self.A
+        self.K_i = K_i
+        self.P_i = P_i
 
     def get_control(self, x, i):
         # TODO: Implement code for mapping states to actions using the
         # pre-computed K_i and P_i matrices 
-        return None
+        control = -1 * self.K_i[i] @ x
+        return control
 
 class DoubleIntegrator(object):
     def __init__(self, dt):
@@ -47,7 +55,12 @@ class DoubleIntegrator(object):
     
     def get_system(self):
         # TODO: Return A, B, Q, R for this system
-        return None, None, None, None
+        A = np.array([[1, self.dt], [0, 1]])
+        B = np.array([[0], [self.dt]])
+        # Need some Q and R to get sum of 2 diagnol matrix to obtain x^2 + v^2 + u^2
+        Q = np.array([[1,0],[0,1]])
+        R = np.array([[1]])
+        return A, B, Q, R
 
 class PendulumBalance(object):
     def __init__(self, dt):
